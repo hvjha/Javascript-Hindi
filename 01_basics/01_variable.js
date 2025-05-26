b3ed59ff7e1eaeea9d5f3a6be9203a772472e75d
Harsh Vardhan Jha <119492741+hvjha@users.noreply.github.com>
2025-05-26 17:19:56 +0000

    DataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 14456
+let accountEmail = "ajha@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2;
+accountEmail = "aja25770@gmail.com"
+accountPassword = "12345678"
+accountCity = "delhi"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
