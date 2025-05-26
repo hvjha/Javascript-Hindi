e00e369c20d25f963ee2829eff1b14e0f246abae
Harsh Vardhan Jha <119492741+hvjha@users.noreply.github.com>
2025-05-26 18:02:33 +0000

    datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score = "33ar5"
+// let score = null
+// let score 
+// let score = true
+let score = "aman"
+
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1;
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true, 0 =>false
+// " " => false
+// "aman" => true
+
+let somerNumber = 33
+let stringNumber = String(somerNumber)
+// console.log(somerNumber);
+// console.log( typeof stringNumber);
+
+// ==================OPERATION============================
+let str1 = "Harsh"
+let str2 = "Jha"
+// console.log(str1 + str2);
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(+true);
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
